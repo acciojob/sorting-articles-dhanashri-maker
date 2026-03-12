ea8cbf6109b47d03d5c438dc3add118c6b70ea08
dhanashri-maker <thedhanashrishinde@gmail.com>
2026-03-12 10:38:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+const bands = [
+'The Plot in You',
+'The Devil Wears Prada',
+'Pierce the Veil',
+'Norma Jean',
+'The Bled',
+'Say Anything',
+'The Midway State',
+'We Came as Romans',
+'Counterparts',
+'Oh, Sleeper',
+'A Skylit Drive',
+'Anywhere But Here',
+'An Old Dog'
+];
+
+function removeArticle(str){
+    return str.replace(/^(a |an |the )/i,'').trim();
+}
+
+const sortedBands = bands.sort((a,b)=>{
+    if(removeArticle(a) > removeArticle(b)) return 1;
+    else return -1;
+});
+
+document.querySelector("#band").innerHTML =
+    sortedBands.map(band => `<li>${band}</li>`).join("");
